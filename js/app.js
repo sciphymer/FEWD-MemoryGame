99a91203ccd2691f213c1ab6ec3dfd3571a9d9f2
Vincent Ng <root@Vincent-PC.localdomain>
2018-05-23 09:46:07 +0800

    - added animation for correct and wrong match of the cards - added score calculation reflected by the num of stars

--- js/app.js
+++ js/app.js
@@ -27,6 +27,8 @@ const moves_display = document.querySelector(".moves");
 let move_cnt = 0;
 let beginTime = 0;
 let stopTime = 0;
+let currentTime = 0;
+let star_score = 0;
 
 function startTimer(){
 	beginTime = new Date().getTime();
@@ -73,20 +75,40 @@ function shuffleAndPlaceCard(){
 	gameBoard.appendChild(tempGameBoardDeck);
 }
 
-function wrongCards(){
-	let wrongCards = document.getElementsByClassName("open");
-	for(let i=0; i<wrongCards.length; i++){
-		wrongCards[i].style.backgroundColor;
+function getScore(){
+	star_score = document.querySelector(".fa-star");
+	currentTime = Math.round(new Date().getTime()-beginTime)/1000;
+	//first star
+	if (move_cnt<=10 && currentTime<=20 && matchedCard.length>=2){
+		star_score.classList.add("fa-star-o");
+		star_score.classList.remove("fa-star");
 	}
+	//second star
+	else if (move_cnt<=20 && currentTime<=30 && matchedCard.length>=5){
+		star_score.classList.add("fa-star-o");
+		star_score.classList.remove("fa-star");
+	}
+	//third star
+	else if (move_cnt<=20 && currentTime<=30 && matchedCard.length==8){
+		star_score.classList.add("fa-star-o");
+		star_score.classList.remove("fa-star");
+	}
+	// alert(currentTime);
+
 }
 
-function removeCards(openedCard){
-	for(let i=0;i<openedCard.length;i++){
-		let temp_openedcards = document.getElementsByClassName(openedCard[i]);
-		for (let j=0;j<temp_openedcards.length;j++){
-			temp_openedcards[j].parentNode.classList.remove("open");
-			temp_openedcards[j].parentNode.classList.remove("show");
-		}
+function wrongCardsEffect(){
+	// debugger;
+	let wrongCards = document.getElementsByClassName("open");
+	for(let i=0; i<wrongCards.length; i++){
+		wrongCards[i].classList.add("wrong");
+	}
+
+}
+
+function correctCardsEffect(matchedCards){
+	for(let i=0; i<matchedCards.length; i++){
+		matchedCards[i].parentNode.classList.add("correct");
 	}
 }
 
@@ -100,25 +122,37 @@ function cardAction(event){
 		if(openedCard.length<=2){
 			target.classList.add("open");
 			target.classList.add("show");
-			//add the moves after the card is opened.
-			move_cnt ++;
-			moves_display.textContent = move_cnt;
 
 			openedCard.push(target.childNodes[0].className);
 			//when a pair of cards are opened, check results
 			if(openedCard.length==2){
+
+				//add the moves after the card is opened.
+				move_cnt ++;
+				moves_display.textContent = move_cnt;
+
 				if(openedCard[0]==openedCard[1]){
 					//when 2 cards are match, show match pattern
 					matchedCard.push(target.childNodes[0].className);
 					let temp_matchedCards = document.getElementsByClassName(openedCard[0]);
+					correctCardsEffect(temp_matchedCards);
 					for(let i=0;i<temp_matchedCards.length;i++){
 						temp_matchedCards[i].parentNode.classList.add("match");
-						temp_matchedCards[i].parentNode.classList.remove("open");
 						temp_matchedCards[i].parentNode.classList.remove("show");
+						temp_matchedCards[i].parentNode.classList.remove("open");
 					}
+					setTimeout(function(){
+						for(let i=0;i<temp_matchedCards.length;i++){
+							temp_matchedCards[i].parentNode.classList.remove("correct");
+						}
+					},600);
+
 					//matched and clear the opendedCard array
 					openedCard.pop();
 					openedCard.pop();
+
+					//update the star score on the panel
+					getScore();
 
 					//check if user wins the game
 					if(matchedCard.includes("fa fa-diamond")&&
@@ -130,21 +164,22 @@ function cardAction(event){
 						matchedCard.includes("fa fa-bomb")&&
 						matchedCard.includes("fa fa-leaf")){
 						stopTimer();
+
 						alert("You win the game!!!\n" + "You have used " + Math.round(stopTime-beginTime)/1000 + " sec.");
 					}
-
-
 				} else {
 					// when 2 cards not match, flip cards to backside
 					// 1sec time delay to show the cards before removal
+					wrongCardsEffect();
+					// debugger;
 					setTimeout(function(){
 						for(let i=0;i<openedCard.length;i++){
 							let temp_openedcards = document.getElementsByClassName(openedCard[i]);
 							for (let j=0;j<temp_openedcards.length;j++){
-								temp_openedcards[j].parentNode.classList.remove("open");
 								temp_openedcards[j].parentNode.classList.remove("show");
+								temp_openedcards[j].parentNode.classList.remove("wrong");
+								temp_openedcards[j].parentNode.classList.remove("open");
 							}
-							console.log(openedCard);
 						}
 						openedCard.pop();
 						openedCard.pop();
