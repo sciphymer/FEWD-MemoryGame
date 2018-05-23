66b5784fc2fc502ee7b2781e7834dd05851185db
Vincent Ng <root@Vincent-PC.localdomain>
2018-05-23 17:01:32 +0800

    fixed star showing problem changed star score calculation restrict the click event when the previous event is wrong card

--- js/app.js
+++ js/app.js
@@ -27,11 +27,13 @@ const moves_display = document.querySelector(".moves");
 const playAgainBtn = document.querySelector(".playAgain");
 let move_cnt = 0;
 let beginTime = 0;
+let current_step_time = 0;
+let last_step_time = 0;
 let stopTime = 0;
-let currentTime = 0;
-let star_score = document.getElementsByClassName("fa-star");
-const overlay1 = document.querySelector(".overlay1");
-const overlay2 = document.querySelector(".overlay2");
+let star_score = 0;
+const finishGameBkg_Overlay = document.querySelector(".finishGameBkg_Overlay");
+const finishGameMsg_Overlay = document.querySelector(".finishGameMsg_Overlay");
+let clickInProcess = false;
 
 function startTimer(){
 	beginTime = new Date().getTime();
@@ -80,25 +82,26 @@ function shuffleAndPlaceCard(){
 
 function getScore(){
 
-	currentTime = Math.round(new Date().getTime()-beginTime)/1000;
+	star_score = document.getElementsByClassName("stars");
 	//first star
-	if (move_cnt<=10 && currentTime<=25 && matchedCard.length>=2){
-		star_score[0].classList.add("fa-star-o");
+	if (move_cnt>=36){
+		star_score[0].children[0].firstChild.classList.add("fa-star");
+		star_score[0].children[0].firstChild.classList.remove("fa-star-o");
 	}
 	//second star
-	else if (move_cnt<=15 && currentTime<=30 && matchedCard.length>=5){
-		star_score[1].classList.add("fa-star-o");
+	else if (move_cnt>=26){
+		star_score[0].children[1].firstChild.classList.add("fa-star");
+		star_score[0].children[1].firstChild.classList.remove("fa-star-o");
 	}
 	//third star
-	else if (move_cnt<=20 && currentTime<=35 && matchedCard.length==8){
-		star_score[2].classList.add("fa-star-o");
+	else if (move_cnt>=16){
+		star_score[0].children[2].firstChild.classList.add("fa-star");
+		star_score[0].children[2].firstChild.classList.remove("fa-star-o");
 	}
-	// alert(currentTime);
-
 }
 
 function wrongCardsEffect(){
-	// debugger;
+	//only current 2 cards which are wrong are open
 	let wrongCards = document.getElementsByClassName("open");
 	for(let i=0; i<wrongCards.length; i++){
 		wrongCards[i].classList.add("wrong");
@@ -113,85 +116,94 @@ function correctCardsEffect(matchedCards){
 }
 
 function cardAction(event){
+
 	let target = event.target;
-	//if the click is on the card area
-	if (target.classList == "card"){
-		// let container = document.querySelector(".overlay");
-		// container.classList.add("finish");
-		if(matchedCard.includes(target.childNodes[0].className))
-			return;
-		if(openedCard.length<=2){
-			target.classList.add("open");
-			target.classList.add("show");
 
-			openedCard.push(target.childNodes[0].className);
-			//when a pair of cards are opened, check results
-			if(openedCard.length==2){
+	current_step_time = new Date().getTime();
+	// to ensure the wrong card animation is finished before click a new card,
+	// or else the card status cannot be updated in time
+	if(((current_step_time-last_step_time)/1000)>0.4){
+		//if the click is on the card area
+		if (target.classList == "card"){
 
-				//add the moves after the card is opened.
-				move_cnt ++;
-				moves_display.textContent = move_cnt;
+			if(matchedCard.includes(target.childNodes[0].className))
+				return;
+			if(openedCard.length<=2){
+				target.classList.add("open");
+				target.classList.add("show");
 
-				if(openedCard[0]==openedCard[1]){
-					//when 2 cards are match, show match pattern
-					matchedCard.push(target.childNodes[0].className);
-					let temp_matchedCards = document.getElementsByClassName(openedCard[0]);
-					correctCardsEffect(temp_matchedCards);
-					for(let i=0;i<temp_matchedCards.length;i++){
-						temp_matchedCards[i].parentNode.classList.add("match");
-						temp_matchedCards[i].parentNode.classList.remove("show");
-						temp_matchedCards[i].parentNode.classList.remove("open");
-					}
-					setTimeout(function(){
-						for(let i=0;i<temp_matchedCards.length;i++){
-							temp_matchedCards[i].parentNode.classList.remove("correct");
-						}
-					},600);
+				openedCard.push(target.childNodes[0].className);
+				//when a pair of cards are opened, check results
+				if(openedCard.length==2){
 
-					//matched and clear the opendedCard array
-					openedCard.pop();
-					openedCard.pop();
-
+					//add the moves after the card is opened.
+					move_cnt ++;
+					moves_display.textContent = move_cnt;
 					//update the star score on the panel
 					getScore();
 
-					//check if user wins the game
-					if(matchedCard.includes("fa fa-diamond")&&matchedCard.includes("fa fa-paper-plane-o")&&
-						matchedCard.includes("fa fa-anchor")&&matchedCard.includes("fa fa-bolt")&&
-						matchedCard.includes("fa fa-cube")&&matchedCard.includes("fa fa-bicycle")&&
-						matchedCard.includes("fa fa-bomb")&&matchedCard.includes("fa fa-leaf")){
-
-						stopTimer();
-						let num_star = document.getElementsByClassName("fa-star-o");
-						// debugger;
-						overlay1.style.display = "inline-block";
-						document.querySelector(".move_Count").innerHTML = move_cnt;
-						document.querySelector(".numOfStars").innerHTML = num_star.length;
-						document.querySelector(".playTime").innerHTML = Math.round((stopTime-beginTime)/1000);
-						setTimeout(function(){overlay2.style.display = "inline-block";
-						},300);
-					}
-				} else {
-					// when 2 cards not match, flip cards to backside
-					// 1sec time delay to show the cards before removal
-					wrongCardsEffect();
-					// debugger;
-					setTimeout(function(){
-						for(let i=0;i<openedCard.length;i++){
-							let temp_openedcards = document.getElementsByClassName(openedCard[i]);
-							for (let j=0;j<temp_openedcards.length;j++){
-								temp_openedcards[j].parentNode.classList.remove("show");
-								temp_openedcards[j].parentNode.classList.remove("wrong");
-								temp_openedcards[j].parentNode.classList.remove("open");
-							}
+					//when 2 cards are match, show match effect
+					if(openedCard[0]==openedCard[1]){
+						//add the card pattern to the matchedCard array
+						matchedCard.push(target.childNodes[0].className);
+						let temp_matchedCards = document.getElementsByClassName(openedCard[0]);
+						correctCardsEffect(temp_matchedCards);
+						//update the status of the 2s match cards
+						for(let i=0;i<temp_matchedCards.length;i++){
+							temp_matchedCards[i].parentNode.classList.add("match");
+							temp_matchedCards[i].parentNode.classList.remove("show");
+							temp_matchedCards[i].parentNode.classList.remove("open");
 						}
+						//Since card correct animation has 0.5sec, so class correct need to be delay to remove after that
+						setTimeout(function(){
+							for(let i=0;i<temp_matchedCards.length;i++){
+								temp_matchedCards[i].parentNode.classList.remove("correct");
+							}
+						},600);
+
+						//matched and clear the opendedCard array
 						openedCard.pop();
 						openedCard.pop();
-					},500);
+
+						//check if user wins the game
+						if(matchedCard.includes("fa fa-diamond")&&matchedCard.includes("fa fa-paper-plane-o")&&
+							matchedCard.includes("fa fa-anchor")&&matchedCard.includes("fa fa-bolt")&&
+							matchedCard.includes("fa fa-cube")&&matchedCard.includes("fa fa-bicycle")&&
+							matchedCard.includes("fa fa-bomb")&&matchedCard.includes("fa fa-leaf")){
+
+							stopTimer();
+							let num_star = document.getElementsByClassName("fa-star-o");
+							finishGameBkg_Overlay.style.display = "inline-block";
+							document.querySelector(".move_Count").innerHTML = move_cnt;
+							document.querySelector(".numOfStars").innerHTML = num_star.length;
+							document.querySelector(".playTime").innerHTML = Math.round((stopTime-beginTime)/1000);
+							//since finishGameBkg_Overlay has 0.2sec animation, finishGameMsg_overlay need to delay to start after that
+							setTimeout(function(){finishGameMsg_Overlay.style.display = "inline-block";
+							},300);
+						}
+					}else{
+						// get time to check when can the next event target be triggered
+						last_step_time = new Date().getTime();
+						wrongCardsEffect();
+						// since wrong card effect has 0.4 sec animation, so the flipping of the card to backside should be done after 0.4sec
+						setTimeout(function(){
+							for(let i=0;i<openedCard.length;i++){
+								let temp_openedcards = document.getElementsByClassName(openedCard[i]);
+								for (let j=0;j<temp_openedcards.length;j++){
+									temp_openedcards[j].parentNode.classList.remove("show");
+									temp_openedcards[j].parentNode.classList.remove("wrong");
+									temp_openedcards[j].parentNode.classList.remove("open");
+								}
+							}
+							openedCard.pop();
+							openedCard.pop();
+						},400);
+					}
 				}
-			}else return;
+			}
 		}
-	}else return;
+	}
+
 }
 
 /*
@@ -205,9 +217,11 @@ function cardAction(event){
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
-function removeStars(){
-	for(let i=0;i<star_score.length;i++){
-		star_score[i].classList.remove("fa-star-o");
+function restartStars(){
+	star_score = document.getElementsByClassName("stars");
+	for(let i=0;i<=2;i++){
+		star_score[0].children[i].firstChild.classList.add("fa-star-o");
+		star_score[0].children[i].firstChild.classList.remove("fa-star");
 	}
 }
 
@@ -217,7 +231,7 @@ function newGame(){
 	moves_display.textContent = 0;
 	matchedCard = [];
 	openedCard = [];
-	removeStars()
+	restartStars();
 	//initialize Game Board Deck
 	while(gameBoard.hasChildNodes()){
 		gameBoard.firstChild.remove();
@@ -227,7 +241,6 @@ function newGame(){
 	//set click listeners to the lists of deck
 	gameBoard.addEventListener('click', cardAction);
 	startTimer();
-
 }
 
 //Restart Game
@@ -235,8 +248,8 @@ restartBtn.addEventListener('click',newGame);
 
 //Play Again Button shown on complete game screen
 playAgainBtn.addEventListener('click',function(){
-	overlay2.style.display = "none";
-	overlay1.style.display = "none";
+	finishGameMsg_Overlay.style.display = "none";
+	finishGameBkg_Overlay.style.display = "none";
 	newGame();
 })
 
